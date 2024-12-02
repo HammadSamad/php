ea8257624a9109bff155fa8494c1a154b9e26279
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2024-12-02 12:22:48 +0500

    Update databasefile.sql

--- pdoproducts/databasefile.sql
+++ pdoproducts/databasefile.sql
@@ -1,11 +1,18 @@
 CREATE DATABASE productsdatabase;
+CREATE TABLE categories(
+category_id INT AUTO_INCREMENT PRIMARY KEY,
+category_Name VARCHAR(255)
+);
+
 CREATE TABLE pdo_products (
 prodId INT AUTO_INCREMENT PRIMARY KEY,
 prodName VARCHAR(255),
 prodDesc VARCHAR(255),
-prodPrice INT,
+prodPrice FLOAT,
 prodImage TEXT,
-prodRating FLOAT
+prodRating FLOAT,
+category_id INT, 
+FOREIGN KEY (category_id) REFERENCES categories(categories_id) ON DELETE CASCADE
 );
 
 
